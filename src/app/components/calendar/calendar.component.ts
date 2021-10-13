b4e63049bbf91a397c576217c58e2e22260e5551
lucienbertin <me@lucienbertin.com>
2021-10-13 14:02:43 +0200

    [add] calendar comp takes a input date serving as refence for what week to render

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit, TrackByFunction } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, TrackByFunction } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements OnInit, OnChanges {
   hours: number[] = [];
   days: Date[] = []
+
+  @Input() date?: Date;
+
   constructor() { }
 
   ngOnInit(): void {
+    this._renderLayout(this.date);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.date) {
+      this._renderLayout(changes.date.currentValue);
+    }
+  }
+
+  trackByDay: TrackByFunction<Date> = (index: number, day: Date): number => {
+    return day.getDay();
+  }
+
+  private _renderLayout(referenceDate = new Date()) {
     this.hours = [...Array(24).keys()].map(i => i+1);
     this.days = [...Array(7).keys()].map(i => {
-      const day = new Date();
+      const day = new Date(referenceDate);
       const offset = day.getDay() - i;
       day.setDate(day.getDate() - offset);
       return day;
     })
   }
 
-  trackByDay: TrackByFunction<Date> = (index: number, day: Date): number => {
-    return day.getDay();
-  }
+
 
 }
