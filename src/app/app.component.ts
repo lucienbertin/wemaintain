150010d69aa4e62ed8b1c305b66b2bb35154787a
lucienbertin <me@lucienbertin.com>
2021-10-13 15:08:00 +0200

    [add] some bogus events to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-
-  date = new Date("2021-02-03");
+  events = [
+    { dtStart: new Date("2021-10-03T12:15:00Z"), dtEnd: new Date("2021-10-03T13:00:00Z"), summary: "last week" },
+    { dtStart: new Date("2021-10-13T12:15:00Z"), dtEnd: new Date("2021-10-13T13:00:00Z"), summary: "this week" },
+    { dtStart: new Date("2021-10-14T11:15:00Z"), dtEnd: new Date("2021-10-14T14:00:00Z"), summary: "this week" },
+    { dtStart: new Date("2021-10-23T11:15:00Z"), dtEnd: new Date("2021-10-23T14:00:00Z"), summary: "next week" },
+  ]
+  date = new Date();
 }
